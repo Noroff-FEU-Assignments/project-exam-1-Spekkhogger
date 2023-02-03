3d865b610a59684daf720f14f5ce609de823d451
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2023-02-03 13:54:22 +0100

    add code to index

---
+++ scripts/slider.js
@@ -0,0 +1,41 @@
+const url = "https://sanna.codes/wp-json/wp/v2/posts?per_page=100&_embed";
+
+const sliderDiv = document.querySelector(".latest-blog-slider");
+const rigthButton = document.querySelector(".slider-button rigth");
+const leftButton = document.querySelector(".slider-button left");
+
+const getSlider = async() => {
+    try {
+        const response = await fetch(url); 
+        const data = await response.json();
+        console.log(data);
+
+        for (let i = 0; i < data.length; i++) {
+            if (i === 4) {
+                break;
+            }
+            const blogID = data[i]["id"];
+            const blogTitle = data[i]["title"]["rendered"];
+            const blogImage = data[i]["_embedded"]["wp:featuredmedia"][0]["source_url"];
+            const blogImageAlt = data[i]["_embedded"]["wp:featuredmedia"][0]["alt_text"];
+            const excerpt = data[i]["excerpt"]["rendered"];
+
+            sliderDiv.innerHTML += `
+            <div class="slider-card">
+                <a href="blogspecific.html?post=${blogID}"><img src="${blogImage}" alt="${blogImageAlt}"></a>
+                <div class="slider-card-text">
+                    <a href="blogspecific.html?post=${blogID}"><h2>${blogTitle}</h2></a>
+                </div>
+            </div>
+            `
+
+        }
+    }
+
+    catch(error) {
+        console.log("There was a mistake " + error);
+    
+    }
+}
+
+getSlider();
